a8fbbaceea78ad8acbb2b1046624d366915b98e9
Walter Reyes <desarrolladorweb@grupofajardo.com.ec>
2024-05-31 20:21:26 -0500

    add: se añade funcionalidad de login con google mediante firebase

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,56 +2,88 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks/useForm';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkingAuthenticaction, startGoogleSignIn } from '../../store/auth/thunks';
+import { useMemo } from 'react';
 
 
 export const LoginPage = () => {
+
+  const { status } = useSelector(state => state.auth);
+
+  const dispatch = useDispatch();
+  const { email, password, onInputChange } = useForm({
+    email: 'walter@gmail.com',
+    password: '123456'
+  });
+
+  const isAuthenticating = useMemo(() => status === 'checking', [status]);
+
+  const onSumbit = (e) => {
+    e.preventDefault();
+    console.log({ email, password });
+    dispatch(checkingAuthenticaction(email, password));
+  }
+
+  const onGoogleSignIn = () => {
+    dispatch(startGoogleSignIn());
+    console.log('Google login');
+  }
+
   return (
     <AuthLayout title="Login">
-      <form>
-          <Grid container>
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Correo" 
-                type="email" 
-                placeholder='correo@google.com' 
-                fullWidth
-              />
-            </Grid>
+      <form onSubmit={onSumbit}>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder='correo@google.com'
+              fullWidth
+              name='email'
+              value={email}
+              onChange={onInputChange}
+            />
+          </Grid>
 
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Contraseña" 
-                type="password" 
-                placeholder='Contraseña' 
-                fullWidth
-              />
-            </Grid>
-            
-            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant='contained' fullWidth>
-                  Login
-                </Button>
-              </Grid>
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant='contained' fullWidth>
-                  <Google />
-                  <Typography sx={{ ml: 1 }}>Google</Typography>
-                </Button>
-              </Grid>
-            </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder='Contraseña'
+              fullWidth
+              name='password'
+              value={password}
+              onChange={onInputChange}
+            />
+          </Grid>
 
-
-            <Grid container direction='row' justifyContent='end'>
-              <Link component={ RouterLink } color='inherit' to="/auth/register">
-                Crear una cuenta
-              </Link>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button type='submit' variant='contained' fullWidth disabled={isAuthenticating}>
+                Login
+              </Button>
             </Grid>
-
+            <Grid item xs={12} sm={6}>
+              <Button variant='contained' fullWidth onClick={onGoogleSignIn} disabled={isAuthenticating}>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
           </Grid>
 
 
-        </form>
+          <Grid container direction='row' justifyContent='end'>
+            <Link component={RouterLink} color='inherit' to="/auth/register">
+              Crear una cuenta
+            </Link>
+          </Grid>
+
+        </Grid>
+
+
+      </form>
 
     </AuthLayout>
   )
